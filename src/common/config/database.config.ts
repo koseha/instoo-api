eeddc279e47609bc504b1c3e26fee146e1558d6d
seha <hansaemj20@gmail.com>
2025-07-28 18:26:03 +0900

    rds pem 변경

--- src/common/config/database.config.ts
+++ src/common/config/database.config.ts
@@ -27,9 +27,7 @@ export const getDatabaseConfig = (configService: ConfigService): TypeOrmModuleOp
       isProduction && useSSL
         ? {
             ca: fs
-              .readFileSync(
-                path.join(__dirname, "..", "..", "..", "..", "rds-combined-ca-bundle.pem"),
-              )
+              .readFileSync(path.join(__dirname, "..", "..", "..", "..", "rds-ca-bundle.pem"))
               .toString(),
           }
         : useSSL,
