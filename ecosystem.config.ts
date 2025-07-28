ac1e1dc6f9f26224407a98d157375f0b4253850c
seha <hansaemj20@gmail.com>
2025-07-28 17:53:52 +0900

    ecosystem.config.js 추가

---
+++ ecosystem.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "nest-app",
+      script: "dist/main.js",
+      exec_mode: "fork", // 👈 이 부분 수정
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "prod",
+      },
+    },
+  ],
+};
